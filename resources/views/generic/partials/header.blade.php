8685ef31fc984cfac05a1c2f4010b6fa85a6eb44
Cody Keith <cody@swirly.io>
2016-06-04 20:47:14 -0500

    test

--- resources/views/generic/partials/header.blade.php
+++ resources/views/generic/partials/header.blade.php
@@ -1,4 +1,4 @@
 <header>
 <img src="/img/logo.png" class="logo">
-<div class="nav_launcher"><i class="fa fa-bars" aria-hidden="true"></i></div>
+<div class="nav_launcher"><i class="fa fa-bars" aria-hidden="true"></i></div>a
 </header>
