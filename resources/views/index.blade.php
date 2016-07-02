27a168f1105cf36a06bdfbd190d3c7fe958bd6d0
Cody Keith <cody@swirly.io>
2016-07-02 01:56:59 -0500

    mission update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -133,7 +133,7 @@
 	<div class="popup">
 		<h2>Here i am</h2>
 		<a class="close" href="#">&times;</a>
-		<div class="content">
+		<div class="content-pop">
 			Thank to pop me out of that button, but now i'm done so you can close this window.
 		</div>
 	</div>
